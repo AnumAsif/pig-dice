ba065ecfc43290bc96e373c6b67876f9187af8a7
Anum Asif <anum@cockar.com>
2018-12-17 08:56:37 +0300

    Add a function to goto the page where users will enter their names

--- js/scripts.js
+++ js/scripts.js
@@ -2,26 +2,96 @@ function Player(name,score){
   this.name = name;
   this.score = score;
 }
+function diceValue(){
+  var diceFace = Math.floor(Math.random()*6)+1;
+  return diceFace;
+}
+function diceFace(val){
+  $("#face"+val).show();
+}
+function getPlayerName(){
+  var index =$("#playerTurn").text().indexOf(":");
+  var name = $("#playerTurn").text().slice(0,index);
+  return name;
+}
+function playGame(player, diceValue){
+  if(diceValue == 1){
+
+    tempScore = 0;
+    $(".tempScore").text(tempScore);
+    // $("#scoreOf"+player+":last").replace("<span>"+player.score+"</span>");
+    // $("#scoreOfOne").append("<br>"+"<span>"+player1.score+"</span>");
+    $("#alert").text("Sorry!!No score for this turn!!");
+    $("#pass").attr("disabled",true);
+  //  $("#alert").prepend("<span>"+"Sorry!!No score for this turn!!"+"</span>");
+    $("#playerTurn").text("");
+
+    return false
+    // $("#play").attr("disabled",true);
+  }else{
+    $("#alert").text("");
+    tempScore+=diceValue;
+    $(".tempScore").text(tempScore);
+    // player1.score=ScoreOne;
+    return true;
+  }
+}
+var tempScore=0;
+var player1;
+var player2;
 $(document).ready(function(){
+
   $("#playButton").click(function(){
     $(".gameRules").hide();
     $(".formDiv").show();
   });
 
-  var player1;
-  var player2;
-  alert("hiiii");
   $("form").submit(function(event){
-    alert("hello");
     event.preventDefault();
-    alert("hi");
-    $(".gameRules").hide()
-    $(".formDiv").hide();
-    $("#startGame").show();
-    player1= new Player($("#playerOne").val(),0);
-    player2 = new Player($("#playerTwo").val(),0);
-    $("#playerOneName").append(player1.name);
-    $("#playerTwoName").append(player2.name);
+    if($("#playerOne").val().length == 0 || $("#playerTwo").val().length == 0){
+      alert("Enter name of both players");
+    }else{
+
+      $(".gameRules").hide()
+      $(".formDiv").hide();
+      $("#startGame").show();
+      player1= new Player($("#playerOne").val(),0);
+      player2 = new Player($("#playerTwo").val(),0);
+      $("#playerOneName").append("<br>"+"<span>"+player1.name+"</span>");
+      $("#playerTwoName").append("<br>"+"<span>"+player2.name+"</span>");
+      $("#scoreOfPlayer1").append("<span>"+player1.score+"</span>");
+      $("#scoreOfPlayer2").append("<span>"+player2.score+"</span>");
+      $("#playerTurn").prepend("<span>"+player1.name+": It's your turn!!"+"</span>");
+      if($("#columnOne").height()>$("#columnTwo").height()){
+        $('#columnTwo').height($("#columnOne").height());
+      }else{
+        $('#columnOne').height($("#columnTwo").height());
+      }
+    }
+
+
   });
+
+
+
+  
+    // if(diceFaceValue == 1){
+    //   $(".tempScore").text("");
+    //   $("#scoreOfOne:first").replaceWith("<span>"+player1.score+"</span>");
+    //   // $("#scoreOfOne").append("<br>"+"<span>"+player1.score+"</span>");
+    //   $("#alert").text("Sorry!!No score for this turn!!");
+    // //  $("#alert").prepend("<span>"+"Sorry!!No score for this turn!!"+"</span>");
+    //   $("#playerTurn").text("");
+    //   $("#playerTurn").prepend("<span>"+player2.name+": It's your turn!!"+"</span>");
+    //   // $("#play").attr("disabled",true);
+    // }else{
+    //   $("#alert").text("");
+    //   player1.score+=diceFaceValue;
+    //   $(".tempScore").append(diceFaceValue +"+");
+    //   // player1.score=ScoreOne;
+    //
+    // }
+
+
 
 });
